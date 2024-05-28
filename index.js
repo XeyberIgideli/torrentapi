1473267f685a3715cb6d73858a6485648286fd2c
Igidali <d3facerx@gmail.com>
2024-05-28 20:10:34 +0400

    fixed a typo

--- index.js
+++ index.js
@@ -1,12 +1,9 @@
-// const torrentSource = require('./torrentSource')  
-// const initApi = require('./initApi')
-// const path = require('path')
 import path from 'path'
 import url from 'url' 
 import initApi from './initApi.js'
 
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename); 
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
 export default initApi(path.join(__dirname,'./sources')) 
 
